4950ddf2147abc14050ba39dbdbd047dab516671
Federico Iosue <federico@iosue.it>
2021-10-31 16:26:39 +0100

    Added delay to wait page loading completion
    First attempt. There is no way to perform a generic check for every kind of site, without waiting for specific component.

--- src/main/kotlin/it/feio/kotlin/sitepreviewer/service/impl/FirefoxWebDriverService.kt
+++ src/main/kotlin/it/feio/kotlin/sitepreviewer/service/impl/FirefoxWebDriverService.kt
@@ -1,16 +1,18 @@
 package it.feio.kotlin.sitepreviewer.service.impl
 
 import it.feio.kotlin.sitepreviewer.service.WebDriverService
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import lombok.extern.slf4j.Slf4j
 import org.openqa.selenium.OutputType
-import org.openqa.selenium.Point
 import org.openqa.selenium.TakesScreenshot
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.io.File
-import java.io.IOException
+
 
 @Service
 @Slf4j
@@ -23,15 +25,19 @@ class FirefoxWebDriverService(
     init {
         System.setProperty("webdriver.gecko.driver", webDriverPath)
         driver = FirefoxDriver()
-        driver.manage().window().position = Point(-2000, 0)
     }
 
-    @Throws(IOException::class)
     override fun peek(url: String) {
-        driver.manage().deleteAllCookies()
-        driver[url]
-        val scrFile: File = (driver as TakesScreenshot).getScreenshotAs(OutputType.FILE)
-        println("Took Screenshot for $url and saved as $scrFile")
+        peekDelayed(url)
     }
+
+    fun peekDelayed(url: String) = runBlocking {
+        launch {
+            driver.manage().deleteAllCookies()
+            driver[url]
+            delay(2000L)
+            val scrFile: File = (driver as TakesScreenshot).getScreenshotAs(OutputType.FILE)
+            println("Took Screenshot for $url and saved as $scrFile")
+        }
     }
 }
